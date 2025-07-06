806a80ed3d9c26c0b9337eea068d14f4c4c1ab43
jeswanth <jachu.k1212@gmail.com>
2025-07-06 17:46:31 +0530

    FullEdited

--- babai-bandi/src/pages/Location.jsx
+++ babai-bandi/src/pages/Location.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/responsive-Location.css';
-import FoodTruck from "../asset/game-icons_food-truck.png"
+import FoodTruck from "../asset/game-icons_food-truck.png";
 
 function Location() {
   return (
