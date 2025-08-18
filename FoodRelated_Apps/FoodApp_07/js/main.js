f6f3f1518295edcf3453905ca77fa1a50dd13735
jeswanth <jachu.k1212@gmail.com>
2025-08-18 10:21:00 +0530

    BabaiBandi_Corrections

---
+++ FoodRelated_Apps/FoodApp_07/js/main.js
@@ -0,0 +1,29 @@
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 50) {
+        $("#topMenu").addClass("scrolled");
+    } else {
+        $("#topMenu").removeClass("scrolled");
+    }
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        // loop: true,
+        margin: 20,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 8000,
+        // responsive: {
+        //     0: { items: 1 },     // mobile
+        //     600: { items: 2 },   // tablet
+        //     1000: { items: 3 },   // desktop    
+        // }
+    });
+
+    // Mobile Menu Toggle
+    // $(".menu-toggle").click(function () {
+    //     $(".menu").toggleClass("active");
+    //     $(this).find("i").toggleClass("fa-bars fa-times");
+    // });
+});
+
